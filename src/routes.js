b43781080577ebf0ca2b50115a4bbef2dd2dc18c
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-02-18 14:18:21 +0800

    update

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,35 @@ const router = Router()
 // STATIC
 router.get('/', (await import('./controllers/static/home.js')).default)
 
+// AUTH | API
+
+
+
+// AUTH | PAGE
+
+
+
+// POST | API
+
+
+
+// POST | PAGE
+
+
+
+// PROFILE | API
+
+
+// PROFILE | PAGE
+
+
+
+
+// COMMUNITY | API
+
+
+// COMMUNITY | PAGE
+
+
+
 export default router
