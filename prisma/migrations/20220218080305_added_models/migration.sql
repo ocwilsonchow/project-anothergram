0fd3c56adca042937904b90a198f82d0c0fc2b30
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-02-18 16:05:16 +0800

    updated prisma

---
+++ prisma/migrations/20220218080305_added_models/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "avatar" TEXT,
+    "location" TEXT,
+    "hobbies" TEXT,
+    "website" TEXT,
+    "occupation" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "imageURL" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "postId" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/controllers/api/auth/signup.js
@@ -0,0 +1,49 @@
+import yup from 'yup'
+import bcrypt from 'bcrypt'
+import _ from 'lodash'
+
+import prisma from '../../_helpers/prisma.js'
+import handleErrors from '../../_helpers/handle-errors.js'
+import uploadFileAsync from '../../_helpers/upload-file.js'
+
+const signupSchema = yup.object({
+  email: yup.string().email().required().test({
+    message: () => 'Email already exists',
+    test: async (value) => {
+      try {
+        await prisma.user.findUnique({ where: { email: value }, rejectOnNotFound: true })
+        return false
+      } catch (err) {
+        return true
+      }
+    }
+  }),
+  password: yup.string().min(6).required(),
+  passwordConfirmation: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required(),
+  avatar: yup.mixed()
+})
+
+const controllersApiAuthSignup = async (req, res) => {
+  try {
+    const { body } = req
+    const verifiedData = await signupSchema.validate(body, { abortEarly: false, stripUnknown: true })
+    await uploadFileAsync(verifiedData, req)
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: verifiedData.email,
+        avatar: verifiedData.avatar || 'https://lab-restful-api.s3.ap-northeast-2.amazonaws.com/profile.jpeg',
+        passwordHash: await bcrypt.hash(verifiedData.password, 10)
+      }
+    })
+
+    req.session.user = { id: newUser.id }
+    await req.session.save()
+
+    return res.status(201).json(_.omit(newUser, ['passwordHash']))
+  } catch (err) {
+    return handleErrors(res, err)
+  }
+}
+
+export default controllersApiAuthSignup
