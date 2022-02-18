54f4d180af5553f112651373d7ac4c8f5c7032f7
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-02-18 15:05:48 +0800

    u

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,8 @@ router.get('/', (await import('./controllers/static/home.js')).default)
 // router.delete('/api/auth/logout', (await import('./controllers/api/auth/logout.js')).default)
 
 // AUTH | PAGE
-
+router.get('/auth/signup', (await import('./controllers/pages/auth/signup.js')).default)
+router.get('/auth/login', (await import('./controllers/pages/auth/login.js')).default)
 
 
 // POST | API
