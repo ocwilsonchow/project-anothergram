5cf8372fcbf3e6de4e4529754dea7b990ca84d20
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-02-21 10:10:18 +0800

    update pages

--- src/routes.js
+++ src/routes.js
@@ -15,32 +15,25 @@ router.delete('/api/auth/logout', (await import('./controllers/api/auth/logout.j
 router.post('/api/my/posts/create',authenticateUser('json'), (await import('./controllers/api/my/posts/create.js')).default)
 router.post('/api/my/posts/index',authenticateUser('json'), (await import('./controllers/api/my/posts/index.js')).default)
 
-
-
 // API | PROFILE
 
 
 // API | COMMUNITY
 
 
-
 // PAGE | AUTH
 router.get('/auth/signup', (await import('./controllers/pages/auth/signup.js')).default)
 router.get('/auth/login', (await import('./controllers/pages/auth/login.js')).default)
 
-
 // PAGE | POST
 router.get('/posts', (await import('./controllers/pages/posts/index.js')).default)
 router.get('/posts/:id', (await import('./controllers/pages/posts/show.js')).default)
-
 
 // PAGE | MY POST
 router.get('/my/posts', (await import('./controllers/pages/my/posts/index.js')).default)
 router.get('/my/posts/create', (await import('./controllers/pages/my/posts/create.js')).default)
 router.get('/my/posts/:id', (await import('./controllers/pages/my/posts/show.js')).default)
 router.get('/my/posts/:id/edit', (await import('./controllers/pages/my/posts/edit.js')).default)
-
-
 
 // PAGE | PROFILE
 router.get('/my/profile', (await import('./controllers/pages/my/profile/show.js')).default)
