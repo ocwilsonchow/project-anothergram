273ae1bb4ce86d4929874f59ceafbfafc494f387
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-02-25 17:26:12 +0800

    updated profile show posts ejs and api controller

--- src/controllers/api/profile/show.js
+++ src/controllers/api/profile/show.js
@@ -1,21 +1,40 @@
-import _ from 'lodash'
+import _ from "lodash";
 
-import prisma from '../../_helpers/prisma.js'
-import handleErrors from '../../_helpers/handle-errors.js'
+import prisma from "../../_helpers/prisma.js";
+import handleErrors from "../../_helpers/handle-errors.js";
 
 const controllersApiProfileUpdate = async (req, res) => {
-  console.log(req.params.id)
+  console.log(req.params.id);
   try {
-    const userId = req.params.id
+    const userId = req.params.id;
     const foundUser = await prisma.user.findUnique({
       where: {
-        id: Number(userId)
-      }, rejectOnNotFound: true })
+        id: Number(userId),
+      },
+      include: {
+        post: {
+          orderBy: {
+            createdAt: "desc",
+          },
+          include: {
+            comment: true,
+            user: {
+              select: {
+                username: true,
+                avatar: true,
+                id: true,
+              },
+            },
+          },
+        },
+      },
+      rejectOnNotFound: true,
+    });
 
-    return res.status(201).json(_.omit(foundUser, ['passwordHash']))
+    return res.status(201).json(_.omit(foundUser, ["passwordHash"]));
   } catch (err) {
-    return handleErrors(res, err)
+    return handleErrors(res, err);
   }
-}
+};
 
-export default controllersApiProfileUpdate
+export default controllersApiProfileUpdate;
