f9073a86dfcd403f825de50508e708dbb928ea62
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-03-01 18:10:05 +0800

    u

--- main.js
+++ main.js
@@ -10,21 +10,13 @@ import { ironSession } from 'iron-session/express'
 import { createServer } from "http";
 import { Server } from "socket.io";
 
-
 import parseData from './src/_middlewares/parse-data.js'
 import addUserToLayout from './src/_middlewares/add-user-to-layout.js'
 
-const app = express() // The instance that "host" our server
+const app = express(); // The instance that "host" our server
 const httpServer = createServer(app);
-const PORT = process.env.PORT || 3000 //  The port number our server runs on
-const INDEX = '/index.html';
-
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX))
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-httpServer.listen(3000);
-const io = new Server(httpServer);
+const port = process.env.PORT || 3000 //  The port number our server runs on
+const io = new Server(httpServer, { });
 
 
 io.on("connection", (socket) => {
@@ -93,8 +85,4 @@ app.use(parseData) // parses multi-part to req.body and req.files
 // Defining the routes for our server
 app.use('/', (await import('./src/routes.js')).default)
 
-// Starts the server
-// app.listen(port, () => {
-//   // eslint-disable-next-line
-//   console.log(`App listening at http://localhost:${port}`)
-// })
+httpServer.listen(3000);
