921451a144ada944c4bb1457bbf59bb91438c8c7
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-03-03 10:26:09 +0800

    adding searchbar and fixing create post imageURL bug

--- src/controllers/api/my/posts/create.js
+++ src/controllers/api/my/posts/create.js
@@ -2,11 +2,13 @@ import yup from "yup";
 
 import prisma from "../../../_helpers/prisma.js";
 import handleErrors from "../../../_helpers/handle-errors.js";
+import uploadFileAsync from '../../../_helpers/upload-file.js'
+
 
 const createSchema = yup.object({
   title: yup.string().required(),
   content: yup.string().required(),
-  imageURL: yup.string(),
+  imageURL: yup.mixed(),
   public: yup.boolean().transform((value) => !!value)
 });
 
@@ -14,10 +16,15 @@ const controllersApiMyPostsCreate = async (req, res) => {
   try {
     const { body, session: { user: { id: userId } } } = req
     const verifiedData = await createSchema.validate(body, { abortEarly: false, stripUnknown: true })
+    await uploadFileAsync(verifiedData, req)
+
     const newPost = await prisma.post.create({
       data: {
-        userId,
-        ...verifiedData,
+        userId: userId,
+        title: verifiedData.title,
+        content: verifiedData.content,
+        imageURL: verifiedData.imageURL || '',
+        public: verifiedData.public
       }
     })
     console.log(newPost)
