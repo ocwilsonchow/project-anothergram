26ce3e6e5f0be4d3e0ca8e94f081d74a7da282db
OC WILSON CHOW <SINLONGCHOW@GMAIL.COM>
2022-03-03 12:02:32 +0800

    fixed delete post error

--- src/controllers/api/my/posts/destroy.js
+++ src/controllers/api/my/posts/destroy.js
@@ -3,9 +3,14 @@ import handleErrors from '../../../_helpers/handle-errors.js'
 import checkOwnership from './_check-ownership.js'
 
 const controllersApiWishlistsDestroy = async (req, res) => {
+
   try {
-    const { params: { id } } = req
-    const deletedPost = await prisma.post.delete({ where: { id: Number(id) } })
+    const postId = parseInt(req.query.id)
+    const deletedPost = await prisma.post.delete({ where:
+      {
+      id: postId
+    }
+  })
     return res.status(200).json(deletedPost)
   } catch (err) {
     return handleErrors(res, err)
